f7140af38cc1ce466def5c0f1caa56725eb3a202
Marcelo Barbosa <marceloppb@outlook.com>
2020-12-15 22:30:48 -0300

    atualizando solides backend

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "type": "postgres",
+  "url": process.env.DATABASE_URL,
+
+  "entities": ["dist/modules/**/infra/typeorm/entities/*.js"],
+  "migrations": ["dist/shared/infra/typeorm/migrations/*.js"],
+  "cli": {
+    "migrationsDir": "src/database/migrations"
+  }
+}
